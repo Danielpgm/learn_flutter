b0b9ecffa16353919dfb3eff1612b652818ccaea
DanielM <danielpaskalis.g@gmail.com>
2024-05-21 01:38:58 +0700

    Revert "card element"
    This reverts commit 1daa9bcef7c9f891893621d1d341a62742ea5b86.

--- lib/main.dart
+++ lib/main.dart
@@ -6,44 +6,15 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("Card Sample"),),
-        body: CardExample(),
-      ),
-    );
-  }
-}
-
-class CardExample extends StatelessWidget{
-  const CardExample({super.key});
-
-  @override
-  Widget build(BuildContext context){
-    return Center(
-      child: Card(
-        color: Colors.amber,
-        child: Column(
-         mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            const ListTile(
-              leading: Icon(Icons.album),
-              title: Text("The Enchanted Nightingale"),
-              subtitle: Text("Music by Julie Gable. Lyrics by Sidney Stein."),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                TextButton(onPressed: (){/*...*/}, child: Text("BUY TICKETS")),
-                const SizedBox(width: 5),
-                TextButton(onPressed: (){debugPrint('ini debug');}, child: Text("LISTEN")),
-                const SizedBox(width: 5)
-              ]
-            ),
-          ],
-        )
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
     );
   }
